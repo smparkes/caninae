59e7943394a51ab9bc4451b1e3b9b032394d2bf4
Steven Parkes <smparkes@smparkes.net>
2013-03-16 18:52:18 -0700

    controls

--- app/assets/javascripts/tollerdata/animals.js
+++ app/assets/javascripts/tollerdata/animals.js
@@ -3,32 +3,23 @@
     console.debug.apply(console, arguments);
   }
 
-  //
-  var canvas = document.createElement('canvas');
-
-  // http://code.google.com/p/explorercanvas/wiki/Instructions#Dynamically_created_elements
-  if (!canvas.getContext) G_vmlCanvasManager.initElement(canvas);
-
-  var ctx = canvas.getContext('2d');
-  canvas.width = 1;
-  canvas.height = 1;
-
-  // for simplicity, assume the input is in rgba format
-  function createPixel(r, g, b, a) {
-    ctx.fillStyle = 'rgba(' + [r,g,b,a].join() + ')';
-    ctx.fillRect(0,0,1,1);
-    // 'data:image/png;base64,'.length => 22
-    return canvas.toDataURL('image/png','').substring(22);
-  }
-  //
-
   var generations = $.cookie('tollerdata_generations');
-  
+  var show_hips = $.cookie('tollerdata_hips');
+  var show_heart = $.cookie('tollerdata_heart');
+  var show_longevity = $.cookie('tollerdata_longevity');
+
   if (generations == void(0)) {
     $.cookie('tollerdata_generations', (generations = 4));
   }
 
-  generations = 3;
+  if (generations < 1) {
+    generations = 1;
+  }
+  if (generations > 8) {
+    generations = 8;
+  }
+
+  $.cookie('tollerdata_generations', generations);
 
   var _individuals = {}
 
@@ -98,6 +89,32 @@
     return titles;
   }
 
+  var compact = function(actual){
+    var newArray = new Array();
+    for(var i = 0; i<actual.length; i++){
+      if (actual[i]){
+        newArray.push(actual[i]);
+      }
+    }
+    return newArray;
+  }
+
+  var longevity = function(animal) {
+    return "10.5 years";
+  }
+
+  var combine = function(json, a, b) {
+    return clean(compact([clean(json[a]), clean(json[b])]).join(" "));
+  }
+
+  var hips = function(json) {
+    return combine(json, "HIPREGISTRY", "HIPID");
+  }
+
+  var heart = function(json) {
+    return combine(json, "HEARTREGISTRY", "HEARTID");
+  }
+
   var render_titles = function(div, indi, json) {
     div.children(".below").children(".titles").html(titles(json));
   }
@@ -105,6 +122,10 @@
   var render_about = function(div, indi, json) {
     render_name(div, indi, json);
     render_titles(div, indi, json);
+
+    div.children(".below").children(".hips").html(hips(json));
+    div.children(".below").children(".heart").html(heart(json));
+    div.children(".below").children(".longevity").html(longevity(json));
   }
 
   var add_pedigree = function(div, id, generation, first, last) {
@@ -169,10 +190,68 @@
   }
 
   $(function(){
+
+    $("#spinner").spinner({
+      stop: function(){
+        generations = $(this).spinner("value");
+        if (generations < 1) {
+          generations = 1;
+        }
+        if (generations > 8) {
+          generations = 8;
+        }
+        $.cookie('tollerdata_generations', generations);
+        $(".generations .value").html($(this).spinner("value"));
+      }
+    });
+
+    $("#spinner").spinner("value", generations);
+    $(".generations .value").html(generations);
+
     div = $(".new_pedigree").first();
     id = div && div.data("animal-id");
     if (div && id) {
       add_pedigree(div, id);
     }
+
+    $(".hips_button input").
+      prop("checked", show_hips).
+      click(function(){
+        d("clicked");
+        var show_hips = $(this).prop("checked");
+        $.cookie('tollerdata_hips', show_hips);
+        if (show_hips) {
+          $(".hips").show();
+        } else {
+          $(".hips").hide();
+        }
+      }).click().click();
+
+    $(".heart_button input").
+      prop("checked", show_heart).
+      click(function(){
+        d("clicked");
+        var show_heart = $(this).prop("checked");
+        $.cookie('tollerdata_heart', show_heart);
+        if (show_heart) {
+          $(".heart").show();
+        } else {
+          $(".heart").hide();
+        }
+      }).click().click();
+
+    $(".longevity_button input").
+      prop("checked", show_longevity).
+      click(function(){
+        d("clicked");
+        var show_longevity = $(this).prop("checked");
+        $.cookie('tollerdata_longevity', show_longevity);
+        if (show_longevity) {
+          $(".longevity").show();
+        } else {
+          $(".longevity").hide();
+        }
+      }).click().click();
+
   });
 }(jQuery));
